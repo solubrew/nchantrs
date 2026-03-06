71e48d41ebca354a4d084e5daecdfce97e16eaa2
Solu <solubrew@solutionsbrewer.com>
2026-03-06 10:32:19 -0500

    FIX: Set self.app = self.parent in applicationviews.py
    The view was overwriting self.app with QApplication.instance() which
    has no .main attribute. Now uses self.parent (NchantdCape) which has it.

--- nchantrs/views/applicationviews.py
+++ nchantrs/views/applicationviews.py
@@ -52,7 +52,7 @@ class NchantdPantiesView(object):
         self.layout = None
         self.pre_view_init_ran = False
         self.post_view_init_ran = False
-        self.app = pyqt.QApplication.instance()
+        self.app = self.parent  # Use parent (NchantdCape) which has .main attribute
 
     def init_pre_view(self):
         """ """
