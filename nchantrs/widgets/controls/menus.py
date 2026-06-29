4f43ab550b6fc187f0eab03a0b8de09b0a539e88
solubrew <solubrew@solutionsbrewer.com>
2026-06-28 22:54:01 -0400

    cleanup

--- nchantrs/widgets/controls/menus.py
+++ nchantrs/widgets/controls/menus.py
@@ -144,6 +144,7 @@ class NchantdContextMenu(NchantdMenu):
         logma.info(f"Name {name}")
         if name:
             self.name = name
+        #TODO: implement cache of menu
         self.menu_df = self.parent.app.model.get_menu(self.name)
         logma.info(f"Menu Data {self.menu_df.head()}")
         menu_data = convert_df_to_tree(self.menu_df)
